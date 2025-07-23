9f34106e203d7a972b7ea6d81c0db37f57c7cc27
Goldi Kumari <amisha6828@gmail.com>
2025-07-23 23:28:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+# app.py
+
+from flask import Flask, render_template, request, jsonify
+from chatbot.bot import get_bot_response
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get', methods=['POST'])
+def chat():
+    user_input = request.json['message']
+    response = get_bot_response(user_input)
+    return jsonify({'response': response})
+
+if __name__ == '__main__':
+    app.run(debug=True)
